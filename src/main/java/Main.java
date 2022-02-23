40156381b731d5152025564081c7ec73f085c424
ZakharovVN <89448998+ZakharovVN@users.noreply.github.com>
2022-02-24 00:51:09 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+    static final int WORKER_ARRAY_SIZE = 5;
+
+    public static void main(String[] args) {
+
+        Worker worker1 = new Worker("npc1", "officer1", "npc1@mail.ru", "+79163211223", 95000, 55);
+        Worker worker2 = new Worker("npc2", "officer2", "npc2@mail.ru", "+79163211224", 96000, 25);
+        Worker worker3 = new Worker("npc3", "officer3", "npc3@mail.ru", "+79163211225", 94000, 50);
+        Worker worker4 = new Worker("npc4", "officer4", "npc4@mail.ru", "+79163211226", 95000, 22);
+        Worker worker5 = new Worker("npc5", "officer5", "npc5@mail.ru", "+79163211227", 90000, 30);
+
+        worker1.printInfo();
+        worker2.printInfo();
+        worker3.printInfo();
+        worker4.printInfo();
+        worker5.printInfo();
+        System.out.println();
+
+        Worker[] wokrArray = new Worker[WORKER_ARRAY_SIZE];
+        wokrArray[0] = new Worker("npc1", "officer1", "npc1@mail.ru", "+79163211223", 95000, 55);
+        wokrArray[1] = new Worker("npc2", "officer2", "npc2@mail.ru", "+79163211224", 96000, 25);
+        wokrArray[2] = new Worker("npc3", "officer3", "npc3@mail.ru", "+79163211225", 94000, 50);
+        wokrArray[3] = new Worker("npc4", "officer4", "npc4@mail.ru", "+79163211226", 95000, 22);
+        wokrArray[4] = new Worker("npc5", "officer5", "npc5@mail.ru", "+79163211227", 90000, 30);
+        for (Worker worker : wokrArray) {
+            if (worker.getAge() >= 40) {
+                worker.printInfo();
+            }
+        }
+
+    }
+}
+
+
---
+++ src/main/java/Worker.java
@@ -0,0 +1,32 @@
+class Worker {
+    private String name;
+    private String post;
+    private String mail;
+    private String phone;
+    private int money;
+    private int age;
+
+
+    public Worker(String name, String post, String mail, String phone, int money, int age) {
+        this.name = name;
+        this.post = post;
+        this.mail = mail;
+        this.phone = phone;
+        this.money = money;
+        this.age = age;
+
+    }
+    public int getAge(){
+        return age;
+    }
+
+
+    void printInfo() {
+        System.out.println(this);
+    }
+
+    public String toString() {
+        return String.format("ФИО: " + name + "; Должность: " + post + "; email: " + mail + "; Телефон: " + phone + "; Зарплата: " + money + "; Возраст: " + age);
+
+    }
+}
